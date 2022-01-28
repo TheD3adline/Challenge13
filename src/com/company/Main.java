a5dae9e92adb1fc5d58a64c14fb54508802d7329
MichaelFessler-Desktop <michaelfessler@gmx.at>
2022-01-28 12:53:50 +0100

    tidied the code and added an output message to the console

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,24 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
 
-    int[][] field = new int[3][3];
-    field[0][0] = 0;
-    field[0][1] = 0;
-    field[0][2] = 0;
-    field[1][0] = 0;
-    field[1][1] = 0;
-    field[1][2] = 0;
-    field[2][0] = 0;
-    field[2][1] = 0;
-    field[2][2] = 0;
+    int[][] field = {{1, 2, 3},
+                     {3, 2, 1},
+                     {1, 1, 1}};
+
     boolean cond;
-    cond = check(field);
-    System.out.println(cond);
+    cond = winCheck(field);
+    System.out.println("Is there three times the same number in one of the lines on the field? " + cond);
 
     }
 
-    //method checks the victory condition
-    public static boolean check(int[][] fieldCheck) {
+    //method that checks the victory condition
+    public static boolean winCheck(int[][] fieldCheck) {
         if((fieldCheck[0][0] == fieldCheck[0][1] && fieldCheck[0][1] == fieldCheck[0][2]) && fieldCheck[0][0] != 0) {
             return true;
         } else if((fieldCheck[1][0] == fieldCheck[1][1] && fieldCheck[1][1] == fieldCheck[1][2]) && fieldCheck[1][0] != 0) {
